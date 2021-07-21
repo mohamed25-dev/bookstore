579533cc9e52dc88e774cba52a7a42ed785518f1
mohamed <mohammed2571994@gmail.com>
2021-07-21 14:01:28 +0300

    Add publishers and authors pages

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -7,17 +7,28 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    public function index()
+    {
+        $authors = Author::all()->sortBy('name');
+        $title = "المؤلفون";
+
+        return view('authors.index', compact('authors', 'title'));
+    }
+
+    public function search (Request $request)
+    {
+        $authors = Author::where('name', 'LIKE', '%' . $request->term . '%')->paginate(12);
+        $title = " عرض نتائج البحث عن" . " : " . $request->term;
+
+        return view('authors.index', compact('authors', 'title'));
+    }
+    
     public function getByAuthor (Author $author)
     {
         $books = $author->books()->paginate(12);
         $title = " عرض كتب المؤلف " . " : " . $author->name;
 
         return view('gallery', compact('books', 'title'));
-    }
-
-    public function index()
-    {
-        //
     }
 
     public function create()
--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -7,77 +7,56 @@ use Illuminate\Http\Request;
 
 class PublisherController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $publishers = Publisher::all()->sortBy('name');
+        $title = "الناشرون";
+
+        return view('publishers.index', compact('publishers', 'title'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function search (Request $request)
+    {
+        $publishers = Publisher::where('name', 'LIKE', '%' . $request->term . '%')->paginate(12);
+        $title = " عرض نتائج البحث عن" . " : " . $request->term;
+
+        return view('publishers.index', compact('publishers', 'title'));
+    }
+
+    public function getByPublisher (Publisher $publisher) 
+    {
+        $books = $publisher->books()->paginate(12);
+        $title = " عرض كتب الناشر " . " : " . $publisher->name;
+
+        return view('gallery', compact('books', 'title'));
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Publisher  $publisher
-     * @return \Illuminate\Http\Response
-     */
     public function show(Publisher $publisher)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Publisher  $publisher
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Publisher $publisher)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Publisher  $publisher
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Publisher $publisher)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Publisher  $publisher
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Publisher $publisher)
     {
         //
